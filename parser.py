d424e7a09cdcf33177acb1ca73fdd9ac49bd0ccd
Helga Derz <ovbukanova@mail.ru>
2015-11-29 20:39:51 +0300

    парсер ленты закончен, косяк в удалении тэгов

--- parser.py
+++ parser.py
@@ -84,7 +84,6 @@ class Base:
 class SimpleSites(Base):
     # проходим по спискам со статьями (метадата здесь)
     def scrolling_pages(self, page, date):
-
         list_daily_news = self.expr_for_article.findall(page)
         list_of_times = self.expr_for_time.findall(page)
         temp_list_news_metadata = []
@@ -241,7 +240,6 @@ class Kommersant(SimpleSites):
 
     # получаем текст статьи
     def get_text(self, url):
-
         text = []
         article = self.open_site(self.main_site + url, self.timeout).decode('cp1251').encode('utf-8')
 
@@ -251,7 +249,6 @@ class Kommersant(SimpleSites):
         return '\n'.join(text)
 
     def get_news(self, since, by):
-
         list_of_days = self.make_days_list(since, by)[0]
         list_daily_news_with_metadata = []
 
@@ -403,7 +400,7 @@ class Interfax(SimpleSites):
 # =========================================================================================================
 #                Л Е Н Т А
 # =========================================================================================================
-class Lenta(SimpleSites):
+class Lenta(Base):
     main_site = 'http://lenta.ru'
 
     timeout = 50
@@ -412,24 +409,66 @@ class Lenta(SimpleSites):
                '/rubrics/style/', '/rubrics/travel/', '/rubrics/life/']
 
     # регулярка для ссылок на статьи
-    expr_for_article = re.compile('<h3><a href="/news/[^А-я]+">')
+    expr_for_article = re.compile('<h3><a href="(/news/[^А-я]+)">')
 
     # регулярка для заголовка
     expr_for_text = re.compile('<title>.+</title>')
 
     # регулярка для тела статьи
-    expr_for_body = re.compile('', re.DOTALL)
+    expr_for_body = re.compile('itemprop="articleBody">(.+)<section class="b-topic__socials">', re.DOTALL)
 
-    # регулярка для "материалов по теме" в теле статьи (их нжно вырезать)
-    expr_for_useless = re.compile('')
+    # регулярка для "материалов по теме" в теле статьи (их нужно вырезать)
+    expr_for_useless = re.compile('<aside class=.+</aside>'), \
+            re.compile('<script.+</script>', re.DOTALL), \
+            re.compile('<div class="eagleplayer".+8".>', re.DOTALL)
 
     # регулярка для времени
-    expr_for_time = re.compile('<span class="time">([0-9]+:[0-9]+)</span> [0-9]+ [а-я]+ [0-9]+</span></div><div class="titles"><h3><a href="/news/')
+    expr_for_time = re.compile('pubdate=""> *([0-9]+:[0-9]+),')
+
+    # получаем текст статьи (с метадатой)
+    def get_text_metadata(self, url, date):
+
+        text = []
+        article = self.open_site(self.main_site + url, self.timeout)
+
+        text.extend(self.expr_for_text.findall(article))
+        body = self.expr_for_body.findall(article)[0]
+
+        for expr in self.expr_for_useless:
+            findings = expr.findall(body)
+            for finding in findings:
+                body.replace(finding, '')  # это не работает. и нет, я не тупая
+
+        text.append(body)
+
+        return ['\n'.join(text), self.expr_for_time.findall(article)[0], date]
+
+    def get_news(self, since, by):
+
+        list_of_days = self.make_days_list(since, by)[0]
+        list_daily_news_with_metadata = []
+
+        for index in range(len(list_of_days)):
+            # нужная дата
+            day, month, year = self.parsing_date(list_of_days[index])
+            date = list_of_days[index][2] + '.' + list_of_days[index][1] + '.' + list_of_days[index][0]
+
+            for rubric in self.rubrics:
+                # общий сайт, где собраны все новости одного дня
+                site_list_daily_news = self.main_site + rubric + year + '/' + month + '/' + day
+
+                # собираем статьи с первой страницы
+                page = self.open_site(site_list_daily_news, self.timeout)
+                list_daily_news = self.expr_for_article.findall(page)
+
+                for url in list_daily_news:
+                    try:
+                        list_daily_news_with_metadata.append(self.get_text_metadata(url, date))
+                    except:
+                        logging.error(url)
+
+        return list_daily_news_with_metadata
 
 
-
-
-a = Interfax()
+a = Lenta()
 lt = a.get_news('22.11.2015', '23.11.2015')
-print len(lt)
-print lt[10][0]
